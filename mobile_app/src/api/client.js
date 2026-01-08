c9c7beb7997851f34b747d6e7aac12b5cf4933f6
Adhi R <adhireghu2005@gmail.com>
2026-01-09 01:42:56 +0530

    Deployment update

--- mobile_app/src/api/client.js
+++ mobile_app/src/api/client.js
@@ -3,11 +3,7 @@ import { Platform } from 'react-native';
 
 // For Android Emulator, use 10.0.2.2. For iOS Simulator, use localhost.
 // For physical devices, replace with your machine's LAN IP.
-const DEV_URL = Platform.select({
-    android: 'http://192.168.0.101:8000',
-    ios: 'http://192.168.0.101:8000',
-    default: 'http://192.168.0.101:8000',
-});
+const DEV_URL = 'https://rntbci-mobile.onrender.com';
 
 const client = axios.create({
     baseURL: DEV_URL,
